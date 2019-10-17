37fedc24f67246ad2cc839a857d8cb7db791b9f8
goncalovalente <goncalo.valente@marionete.co.uk>
2019-10-17 17:10:40 +0100

    Add functionality. Fix failing tests

--- bq_external_table/main.py
+++ bq_external_table/main.py
@@ -8,13 +8,15 @@ log = logger.get_logger()
 
 def main():
     arguments = args_parser.parse_arguments()
-    log_location, credentials_file_path, dataset_name, buckets_and_tables = set_run_variables(
+    log_location, credentials_file_path, dataset_name, buckets = set_run_variables(
         json_config=utils_functions.load_json_config(path=arguments.json_config))
     logger.setup_logger(log_location)
     gcp_interfacer.authenticate_gcp(credentials_file_path=credentials_file_path)
     bigquery_client = gcp_interfacer.get_bigquery_client()
-    for bucket, table in buckets_and_tables.items():
-        gcp_interfacer.load_bucket_data_into_bigquery_external_table(bigquery_client=bigquery_client,
-                                                                     dataset_name=dataset_name,
-                                                                     bucket_name=bucket,
-                                                                     table_id=table.replace("-", "_"))
+    for bucket, blobs in buckets.items():
+        for blob_details in blobs:
+            gcp_interfacer.load_bucket_data_into_bigquery_external_table(bigquery_client=bigquery_client,
+                                                                         dataset_name=dataset_name,
+                                                                         bucket_name=bucket,
+                                                                         table_id=blob_details.get("table_name"),
+                                                                         blob_prefix=blob_details.get("blob_prefix"))
